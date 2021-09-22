06d8e3b76a55a81144292c6bf32285025d04e842
Elise Brown <elisemarie88@gmail.com>
2021-09-22 14:57:44 -0600

    backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  extend Divise::Models
+  extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
